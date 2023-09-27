ec38de33f5c898432b6abd08936af88ac39fd9d1
FatttSnake <fatttsnake@gmail.com>
2023-09-28 01:36:16 +0800

    Add @EnableTransactionManagement

--- src/main/kotlin/top/fatweb/api/FatWebApiApplication.kt
+++ src/main/kotlin/top/fatweb/api/FatWebApiApplication.kt
@@ -2,8 +2,10 @@ package top.fatweb.api
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.transaction.annotation.EnableTransactionManagement
 
 @SpringBootApplication
+@EnableTransactionManagement
 class FatWebApiApplication
 
 fun main(args: Array<String>) {
