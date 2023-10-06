243d7654e7c5082927ceb25094f5c3947154ba0c
FatttSnake <fatttsnake@gmail.com>
2023-10-06 15:00:08 +0800

    Hide ExceptionController in swagger

--- src/main/kotlin/top/fatweb/api/controller/ExceptionController.kt
+++ src/main/kotlin/top/fatweb/api/controller/ExceptionController.kt
@@ -1,9 +1,11 @@
 package top.fatweb.api.controller
 
+import io.swagger.v3.oas.annotations.Hidden
 import jakarta.servlet.http.HttpServletRequest
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
+@Hidden
 @RestController
 @RequestMapping("/error")
 class ExceptionController {
