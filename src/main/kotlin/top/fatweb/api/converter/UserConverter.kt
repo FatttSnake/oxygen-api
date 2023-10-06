e085005f73213e922371de757a14681a417061fc
FatttSnake <fatttsnake@gmail.com>
2023-10-07 02:53:28 +0800

    Remove mapstruct

--- src/main/kotlin/top/fatweb/api/converter/UserConverter.kt
+++ src/main/kotlin/top/fatweb/api/converter/UserConverter.kt
@@ -1,14 +1,17 @@
 package top.fatweb.api.converter
 
-import org.mapstruct.Mapper
-import org.mapstruct.Mapping
-import org.mapstruct.Mappings
+import org.springframework.stereotype.Component
 import top.fatweb.api.entity.permission.User
 import top.fatweb.api.param.LoginParam
 
-@Mapper(componentModel = "spring")
-interface UserConverter {
-    @Mappings(Mapping(source = "username", target = "username"), Mapping(source = "password", target = "password"))
-    fun loginParamToUser(loginParam: LoginParam): User
+@Component
+object UserConverter {
+    fun loginParamToUser(loginParam: LoginParam): User {
+        val user = User().apply {
+            username = loginParam.username
+            password = loginParam.password
+        }
 
+        return user
+    }
 }
