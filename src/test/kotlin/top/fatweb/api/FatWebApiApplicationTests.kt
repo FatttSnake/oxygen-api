723bdd4c29fb8d6b8d1e75902ce8475b5629dfca
FatttSnake <fatttsnake@gmail.com>
2023-10-08 17:17:37 +0800

    Recode unit test

--- src/test/kotlin/top/fatweb/api/FatWebApiApplicationTests.kt
+++ src/test/kotlin/top/fatweb/api/FatWebApiApplicationTests.kt
@@ -2,29 +2,33 @@ package top.fatweb.api
 
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
-import org.springframework.boot.test.context.SpringBootTest
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.test.context.junit.jupiter.SpringExtension
 import top.fatweb.api.constant.SecurityConstants
+import top.fatweb.api.util.JwtUtil
 
-@SpringBootTest
+@ExtendWith(SpringExtension::class)
 class FatWebApiApplicationTests {
-
-    @Test
-    fun contextLoads() {
-    }
 
     @Test
     fun removePrefixTest() {
         assertEquals("12312", "Bearer 12312".removePrefix(SecurityConstants.tokenPrefix))
     }
 
+    @Test
+    fun jwtTest() {
+        val jwt = JwtUtil.createJwt("User")
+        assertEquals("User", jwt?.let { JwtUtil.parseJwt(it).subject })
+    }
+
     /*
-        @Test
-        fun addUser(@Autowired userService: IUserService, @Autowired passwordEncoder: PasswordEncoder) {
-            val username = "admin"
-            val rawPassword = "admin"
-            val encodedPassword = passwordEncoder.encode(rawPassword)
-            val user = User(username, encodedPassword)
-            userService.save(user)
-        }
-    */
+            @Test
+            fun addUser(@Autowired userService: IUserService, @Autowired passwordEncoder: PasswordEncoder) {
+                val username = "admin"
+                val rawPassword = "admin"
+                val encodedPassword = passwordEncoder.encode(rawPassword)
+                val user = User(username, encodedPassword)
+                userService.save(user)
+            }
+        */
 }
