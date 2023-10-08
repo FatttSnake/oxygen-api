82c9fcb10fb9b6cb394ccc231459e09ba287bce8
FatttSnake <fatttsnake@gmail.com>
2023-10-08 23:33:59 +0800

    Change the example config path

--- src/main/kotlin/top/fatweb/api/FatWebApiApplication.kt
+++ src/main/kotlin/top/fatweb/api/FatWebApiApplication.kt
@@ -14,12 +14,19 @@ class FatWebApiApplication
 fun main(args: Array<String>) {
     val logger = LoggerFactory.getLogger("main")
 
+    if (!File("data").isDirectory) {
+        if (!File("data").mkdir()) {
+            logger.error("Can not create directory 'data', please try again later.")
+            return
+        }
+    }
+
     if (File("application-config.yml").exists()) {
         runApplication<FatWebApiApplication>(*args)
     } else {
-        logger.warn("File ‘application.yml’ cannot be found in the running path. The configuration file template 'application.example.yml' has been created. Please change the configuration file content and rename it to 'application.yml', and then restart the server.")
+        logger.warn("File ‘application.yml’ cannot be found in the running path. The configuration file template 'application.example.yml' has been created in directory 'data'. Please change the configuration file content, rename it to 'application.yml' and put it in the running path, and then restart the server.")
         FatWebApiApplication::class.java.getResource("/application-config-template.yml")?.readText()?.let {
-            File("application-config.example.yml").writeText(
+            File("data/application-config.example.yml").writeText(
                 it.replace(
                     "\$uuid\$", UUID.randomUUID().toString().replace("-", "")
                 )
