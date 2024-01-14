6cc16adc1d7c6d59bd8a49d80489714f259bb818
FatttSnake <fatttsnake@gmail.com>
2024-01-14 18:59:07 +0800

    Add ER map

--- src/main/resources/db/migration/master/V1_0_0_231031__Add_table_'t_role_group'.sql
+++ src/main/resources/db/migration/master/V1_0_0_231031__Add_table_'t_role_group'.sql
@@ -4,7 +4,7 @@ create table if not exists t_role_group
 (
     id       bigint not null primary key,
     role_id  bigint not null comment '角色',
-    group_id bigint not null comment '群组',
+    group_id bigint not null comment '用户组',
     deleted  bigint not null default 0,
     version  int    not null default 0
 ) comment '中间表-角色-用户组';
