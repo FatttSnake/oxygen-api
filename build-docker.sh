3202bb5422019efa9be7f79b9bb3b264b42da897
FatttSnake <fatttsnake@gmail.com>
2024-09-04 18:12:54 +0800

    Build(build-docker.sh): Optimize script

--- build-docker.sh
+++ build-docker.sh
@@ -1,24 +1,17 @@
 #!/bin/bash
 
-JAR_NAME=`ls target | grep api-|grep -v original`
+JAR_NAME=`ls target | grep oxygen-api- | grep -vE "original|asc|pom|javadoc"`
 JAR_VERSION=${JAR_NAME%.*}
+JAR_VERSION=${JAR_VERSION#*-}
 JAR_VERSION=${JAR_VERSION#*-}
 BUILD_TIME=$(date "+%Y%m%d%H%M%S")
 
 mkdir target/extracted
-java -Djarmode=layertools -jar target/*.jar extract --destination target/extracted
+java -Djarmode=layertools -jar target/${JAR_NAME} extract --destination target/extracted
 
 if [[ "${JAR_VERSION}" =~ ^.*SNAPSHOT$ ]]
 then
-  docker build -t ${DOCKER_HUB_URL}/oxygen-api:snapshot-latest -t ${DOCKER_HUB_URL}/oxygen-api:$JAR_VERSION -t ${DOCKER_HUB_URL}/oxygen-api:$JAR_VERSION-${BUILD_TIME} .
-  cat "${KEYS_PATH}/docker.password" | docker login ${DOCKER_HUB_URL} -u jenkins --password-stdin
-  docker push ${DOCKER_HUB_URL}/oxygen-api:snapshot-latest
-  docker push ${DOCKER_HUB_URL}/oxygen-api:$JAR_VERSION
-  docker push ${DOCKER_HUB_URL}/oxygen-api:$JAR_VERSION-${BUILD_TIME}
+  docker build -t ${DOCKER_HUB_URL}/oxygen-api:snapshot-latest -t ${DOCKER_HUB_URL}/oxygen-api:${JAR_VERSION} -t ${DOCKER_HUB_URL}/oxygen-api:${JAR_VERSION}-${BUILD_TIME} .
 else
-  docker build -t ${DOCKER_HUB_URL}/oxygen-api:latest -t ${DOCKER_HUB_URL}/oxygen-api:$JAR_VERSION -t ${DOCKER_HUB_URL}/oxygen-api:$JAR_VERSION-${BUILD_TIME} .
-  cat "${KEYS_PATH}/docker.password" | docker login ${DOCKER_HUB_URL} -u jenkins --password-stdin
-  docker push ${DOCKER_HUB_URL}/oxygen-api:latest
-  docker push ${DOCKER_HUB_URL}/oxygen-api:$JAR_VERSION
-  docker push ${DOCKER_HUB_URL}/oxygen-api:$JAR_VERSION-${BUILD_TIME}
+  docker build -t ${DOCKER_HUB_URL}/oxygen-api:latest -t ${DOCKER_HUB_URL}/oxygen-api:${JAR_VERSION} -t ${DOCKER_HUB_URL}/oxygen-api:${JAR_VERSION}-${BUILD_TIME} .
 fi
